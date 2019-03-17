9ee6341aa4320d391cb63751d8adf4e80e2f9003
Arseniy Banayev <arseniy.banayev@gmail.com>
2019-03-16 22:40:11 -0400

    All routes lead to the index

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const express = require('express');
 module.exports = {
     app: function () {
         const app = express();
-        const indexPath = path.join(__dirname, 'index.html');
         const publicPath = express.static(path.join(__dirname, 'public'));
 
         app.use('/public', publicPath);
-        app.get('/', function (_, res) { res.sendFile(indexPath) });
+
+        app.get('*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'index.html'));
+        });
 
         return app
     }
