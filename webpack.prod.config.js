f98a3a760db48e11800f12d5f4c4d092b9da73f8
Arseniy Banayev <arseniy.banayev@gmail.com>
2019-03-16 23:10:50 -0400

    Remove the HTML plugin from prod config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
@@ -20,9 +19,6 @@ module.exports = {
   },
 
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
