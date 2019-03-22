c7e779e3db2be7d37cbd67305abe90898dcd5b11
Tomer Chachamu <tomer.chachamu@google.com>
2019-03-22 21:51:45 +0000

    simpler display of romanizatiosn

--- src/components/ChooseCharacters/LetterSet.jsx
+++ src/components/ChooseCharacters/LetterSet.jsx
@@ -7,20 +7,10 @@ import {romanizations} from "../../hangeul";
  * syllables appear in the game by difficulty or frequency/commonness.
  */
 export default class LetterSet extends Component {
-  state = { displayString: '' };
-
-  changeDisplayString(newString) {
-    this.setState({displayString: newString})
-  }
-
   getDisplayString(writingType) {
     return this.props.allowedLetters
       .map(c => writingType === WritingType.HANGEUL ? c : romanizations[c][0])
       .join(' · ');
-  }
-
-  componentWillMount() {
-    this.changeDisplayString(this.getDisplayString(WritingType.HANGEUL));
   }
 
   render() {
@@ -28,16 +18,13 @@ export default class LetterSet extends Component {
       <div
         className={'choose-row'}
         onClick={() => this.props.handleToggleSelect(this.props.name)}
-        onMouseDown={() => this.changeDisplayString(this.getDisplayString(WritingType.ROMANIZATION))}
-        onMouseUp={() => this.changeDisplayString(this.getDisplayString(WritingType.HANGEUL))}
-        onTouchStart={() => this.changeDisplayString(this.getDisplayString(WritingType.ROMANIZATION))}
-        onTouchEnd={() => this.changeDisplayString(this.getDisplayString(WritingType.HANGEUL))}
       >
         <span className={this.props.selected ?
           'glyphicon glyphicon-small glyphicon-check' :
           'glyphicon glyphicon-small glyphicon-unchecked'} />
           {/*There are two spaces here, and categories have one space*/}
-          &nbsp;&nbsp;{this.props.name}: {this.state.displayString}
+          &nbsp;&nbsp;{this.props.name}: {this.getDisplayString(WritingType.HANGEUL)}
+          &nbsp;&nbsp;<span className='romanization'>{this.getDisplayString(WritingType.ROMANIZATION)}</span>
       </div>
     );
   }
